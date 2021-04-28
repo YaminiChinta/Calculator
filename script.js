3c9616e4dd8c88395419103dc6d001dff7c1b1f9
YaminiChinta <67141828+YaminiChinta@users.noreply.github.com>
2021-04-29 00:32:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function dele(){
-    var a=document.getElementById('display').value;
-    document.getElementById('display').value=a.slice(0,a.length-1);
+    var p=document.getElementById('display').value;
+    document.getElementById('display').value=p.slice(0,p.length-1);
 }
