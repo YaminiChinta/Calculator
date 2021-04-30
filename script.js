3c52eb72f8343f4830a7e8e316ec32eac4f5165b
YaminiChinta <67141828+YaminiChinta@users.noreply.github.com>
2021-04-30 12:37:49 +0530

    29-04-2021 assignment

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function dele(){
     var p=document.getElementById('display').value;
     document.getElementById('display').value=p.slice(0,p.length-1);
 }
+
+function calculate(){
+    var a=document.getElementById('display').value;
+    var b=eval(a);
+    if(b==undefined){
+        b=null;
+    }
+    document.getElementById('display').value=b;
+}
